9676b0138d558cf7f19f0d24cf1c84687f60deb1
aditihaiman <ahaiman00@stuy.edu>
2020-02-06 18:44:37 -0500

    debugging octant 7

--- draw.py
+++ draw.py
@@ -2,21 +2,23 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-#    if(x0 > x1):
-#        temp = x1
-#        x1 = x0
-#        x0 = temp
-#        temp = y1
-#        y1 = y0
-#        y0 = temp
+    if(x0 > x1):
+        temp = x1
+        x1 = x0
+        x0 = temp
+        temp = y1
+        y1 = y0
+        y0 = temp
     print(x0, y0, x1, y1)
-    if(x0 == x1): slope = 0
+    if(x1 == x0): slope = 5
+    elif(y0 == y1): slope = 0
     else: slope = (y1-y0)/(x1-x0)
     if(slope > 0):
         if(slope <= 1): octant1( x0, y0, x1, y1, screen, color)
         else: octant2( x0, y0, x1, y1, screen, color)
     else:
         if(slope >= -1): octant8( x0, y0, x1, y1, screen, color)
+        else: octant7( x0, y0, x1, y1, screen, color)
         
 def octant1( x0, y0, x1, y1, screen, color ):
     A = 2*(y1-y0)
@@ -46,13 +48,13 @@ def octant7(x0, y0, x1, y1, screen, color):
     A = (y1-y0)
     B = 2*(x0-x1)
     d = A - B
-    while( y0 <= y1):
+    while( y0 >= y1):
         plot(screen, color, x0, y0)
         if(d > 0):
-            y0 = y0-1
-            d = d - 2*A
-        x0 = x0+1
-        d = d + B
+            x0 = x0+1
+            d = d + 2*A
+        y0 = y0-1
+        d = d - B
 
 def octant8(x0, y0, x1, y1, screen, color):
     A = 2*(y1-y0)
--- main.py
+++ main.py
@@ -5,41 +5,43 @@ from draw import *
 s = new_screen()
 c = [ 0, 255, 0 ]
 
-##octants 1 and 5
-#draw_line(0, 0, XRES-1, YRES-1, s, c)
-#draw_line(0, 0, XRES-1, YRES / 2, s, c)
-#draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
-#
-##octants 8 and 4
-#c[BLUE] = 255;
-#draw_line(0, YRES-1, XRES-1, 0, s, c);
-#draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-#draw_line(XRES-1, 0, 0, YRES/2, s, c);
-#
-##octants 2 and 6
-#c[RED] = 255;
-#c[GREEN] = 0;
-#c[BLUE] = 0;
-#draw_line(0, 0, XRES/2, YRES-1, s, c);
-#draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
-#
-##octants 7 and 3
-#c[BLUE] = 255;
-#draw_line(0, YRES-1, XRES/2, 0, s, c);
-#draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
-#
-##horizontal and vertical
-#c[BLUE] = 0;
-#c[GREEN] = 255;
-#draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
-#draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
-#
+#octants 1 and 5
+draw_line(0, 0, XRES-1, YRES-1, s, c) #(0,0) - (499, 499)
+draw_line(0, 0, XRES-1, YRES / 2, s, c) #(0,0) - (499, 250)
+draw_line(XRES-1, YRES-1, 0, 250, s, c) #(499, 499) - (0, 250)
 
-draw_line(0, 0, 500, 100, s, c);
-draw_line(0, 0, 100, 500, s, c);
-draw_line(0, 0, 500, 0, s, c);
-draw_line(0, 500, 500, 300, s, c);
+#octants 8 and 4
+c[BLUE] = 255;
+draw_line(0, YRES-1, XRES-1, 0, s, c); #(0, 499) - (499, 0)
+draw_line(0, YRES-1, XRES-1, YRES/2, s, c); #(0, 499) - (499, 250)
+draw_line(XRES-1, 0, 0, 250, s, c); #(499, 0) - (0, 250)
 
+#octants 2 and 6
+c[RED] = 255;
+c[GREEN] = 0;
+c[BLUE] = 0;
+draw_line(0, 0, 250, YRES-1, s, c); #(0,0) - (250, 499)
+draw_line(XRES-1, YRES-1, 250, 0, s, c); #(499,499) - (250, 0)
+#draw_line(250, 499, 499, 0, s, c);
+
+#octants 7 and 3
+c[BLUE] = 255;
+draw_line(0, YRES-1, 250, 0, s, c); #(0, 499) - (250, 0)
+draw_line(XRES-1, 0, 250, YRES-1, s, c);
+
+#horizontal and vertical
+c[BLUE] = 0;
+c[GREEN] = 255;
+draw_line(0, 250, 499, 250, s, c);
+draw_line(250, 0, 250, 499, s, c);
+
+#
+#draw_line(0, 0, 500, 100, s, c); #octant 1
+#draw_line(0, 0, 100, 500, s, c); #octant 2
+#draw_line(0, 0, 500, 0, s, c); #octant 1
+#draw_line(0, 500, 500, 300, s, c); #octant 8
+#draw_line(0, 500, 300, 0, s, c); #octant 7
+##draw_line(0, 500, 500, 500, s, c);
 
 display(s)
 save_ppm(s, 'binary.ppm')
